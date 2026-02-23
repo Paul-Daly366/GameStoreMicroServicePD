a6c16b8f46f1d9753d4d8a830ae48b1a3ce153bf
PaulDaly-STUDENT <g00470372@atu.ie>
2026-02-23 12:52:14 +0000

    Initial Commit

---
+++ src/main/java/ie/atu/gamestoremicroservicepd/GameStoreMicroServicePdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.gamestoremicroservicepd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreMicroServicePdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStoreMicroServicePdApplication.class, args);
+    }
+
+}
