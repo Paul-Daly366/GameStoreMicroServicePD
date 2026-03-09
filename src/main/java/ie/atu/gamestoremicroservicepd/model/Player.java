13744752e1607bad1c149ee6eca8845a9de7798d
PaulDaly-STUDENT <g00470372@atu.ie>
2026-03-09 12:55:29 +0000

    Added and coded player object

---
+++ src/main/java/ie/atu/gamestoremicroservicepd/model/Player.java
@@ -0,0 +1,27 @@
+package ie.atu.gamestoremicroservicepd.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Player {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private Long id;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    @NotBlank(message = "Player name cannot be left blank.")
+    private String name;
+
+    //@NotBlank(message = "Player nickname cannot be left blank.")
+    //private String nickname;
+
+    private double credit = 0;
+
+    @NotBlank(message = "Email cannot be left blank.")
+    private String email;
+
+}
