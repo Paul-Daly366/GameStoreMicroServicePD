e2784ce45ddd455edad9886508f81d2f521e81fa
PaulDaly-STUDENT <g00470372@atu.ie>
2026-03-18 10:53:19 +0000

    Added database for player, not finished yet

---
+++ src/main/java/ie/atu/gamestoremicroservicepd/repository/PlayerRepo.java
@@ -0,0 +1,7 @@
+package ie.atu.gamestoremicroservicepd.repository;
+
+import ie.atu.gamestoremicroservicepd.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepo extends JpaRepository<Player, Long> {
+}
