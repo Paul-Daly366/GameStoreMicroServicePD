e0e1bea8be512a9d0372ce5aa8158d9704f1e81d
PaulDaly-STUDENT <g00470372@atu.ie>
2026-03-23 13:06:55 +0000

    Added all databases planned for this sprint, testing and finetuning next commit

--- src/main/java/ie/atu/gamestoremicroservicepd/model/Creator.java
+++ src/main/java/ie/atu/gamestoremicroservicepd/model/Creator.java
@@ -1,6 +1,10 @@
 package ie.atu.gamestoremicroservicepd.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -8,7 +12,10 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Creator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long creatorId;
 
--- src/main/java/ie/atu/gamestoremicroservicepd/service/CreatorService.java
+++ src/main/java/ie/atu/gamestoremicroservicepd/service/CreatorService.java
@@ -1,6 +1,7 @@
 package ie.atu.gamestoremicroservicepd.service;
 
 import ie.atu.gamestoremicroservicepd.model.Creator;
+import ie.atu.gamestoremicroservicepd.repository.CreatorRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,17 +10,21 @@ import java.util.List;
 @Service
 public class CreatorService {
 
-    private final List<Creator> creators = new ArrayList<>();
+    private final CreatorRepo creatorRepo;
+    private List<Creator> creators = new ArrayList<>();
     private long nextId = 1;
 
+    public CreatorService(CreatorRepo creatorRepo) {
+        this.creatorRepo = creatorRepo;
+    }
+
     public Creator addCreator(Creator creator){
-        creator.setCreatorId(nextId++);
-        creators.add(creator);
+        creatorRepo.save(creator);
         return creator;
     }
 
     public List<Creator> getAllCreators(){
-        return creators;
+        return creatorRepo.findAll();
     }
 
 }
